bd4944bbd44ec12c8b8b972fd2745840f719fadb
jmartensen <julius.martensen@dfki.de>
2019-11-02 17:38:12 +0100

    Further testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using DataDrivenDiffEq
 using ModelingToolkit
+using LinearAlgebra
 using Test
 
 @testset "Basis" begin
@@ -22,4 +23,31 @@ using Test
     basis = Basis(g, u, parameters = w)
     f = jacobian(basis)
     @test f([1;1;1], [0.0 0.0]) ≈ [1.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0]
+end
+
+@testset "DMD" begin
+    # Create some linear data
+    A = [0.9 -0.2; 0.0 0.2]
+    y = [[10.; -10.]]
+    for i in 1:10
+        push!(y, A*y[end])
+    end
+    X = hcat(y...)
+    estimator = ExactDMD(X[:,1:end-2])
+    @test isstable(estimator)
+    @test estimator.Ã ≈ A
+    @test eigvals(estimator) ≈ eigvals(A)
+    @test eigvecs(estimator) ≈ eigvecs(A)
+    @test_nowarn dynamics(estimator)
+    @test_throws AssertionError dynamics(estimator, discrete = false)
+    @test_nowarn update!(estimator, X[:, end-1], X[:,end])
+end
+
+
+
+
+@testset "EDMD" begin
+end
+
+@testset "SInDy" begin
 end
