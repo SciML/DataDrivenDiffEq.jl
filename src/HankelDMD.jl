0398bddee8de69d307c540c8b85ea78bc3e002a4
Julius Martensen <julius.martensen@gmail.com>
2019-12-06 21:30:25 +0100

    Add hankel and hankel dmd

--- src/HankelDMD.jl
+++ src/HankelDMD.jl
@@ -37,35 +37,39 @@ function CompanionMatrixDMD(X::AbstractArray, Y::AbstractArray; dt::T = 0.0)  wh
     return ExactDMD(Ã,Λ,ω,W, nothing, nothing)
 end
 
-function hankel(A, m)
-        if m == 0
-                return A
+function hankel(a::AbstractArray, b::AbstractArray; scale::T = 1.0) where T <: Number
+    p = vcat([a; b[2:end]])
+    n = length(b)-1
+    m = length(p)-n+1
+    H = Array{eltype(p)}(undef, n, m)
+    @inbounds for i in 1:n
+        @inbounds for j in 1:m
+            H[i,j] = p[i+j-1]
         end
-
-        x,y = size(A)
-
-        H = Array{eltype(A)}(undef, x*(m+1), y-m)
-
-        for i in 1:m+1
-                H[1+(i-1)*x:i*x, :] = A[:,i:y-(m-i)-1]
-        end
-        return H
+    end
+    mul!(H, norm(H[:, 1])/scale, H)
+    return H
 end
 
-function HankelDMD(X, Y, m)
-        # Delay embedding
-        #D = delay_embedding(X, m)
-        # Companion matrix
-        c = pinv(X)*X[:, end]
-        C = hcat([zeros(eltype(c), 1, length(c)-1); Diagonal(ones(eltype(c), length(c)-1))], c)
+function HankelDMD(X::AbstractArray,  n::Int64, m::Int64)
+    return HankelDMD(X[:, 1:end-1], X[:, 2:end], n, m)
 end
 
-using Plots
-A = rand(3,100)
+function HankelDMD(X::AbstractArray, Y::AbstractArray, n::Int64 , m::Int64)
+    @assert size(X)[2] >= n+m
+    @assert size(X) == size(Y)
+    # TODO Assert size a priori
+    # Double check the method with examples
+    H₀ = [] # Hankel matrices
+    H₁ = []
+    scale = norm(X[:, 1])
+    @inbounds for i in 1:size(X)[1]
+        push!(H₀, hankel(@view(X[i, 1:n]), @view(X[i, n:n+m]), scale = i > 1 ? scale : one(eltype(X))))
+        push!(H₁, hankel(@view(Y[i, 1:n]), @view(Y[i, n:n+m]), scale = i > 1 ? scale : one(eltype(X))))
+    end
 
-c = HankelDMD(A[:, 1:end-1], A[:, 2:end], 1)
-scatter(eigvals(Matrix(c)))
-H = taken(A[:, 1:end-1], 1)
-H2 = taken(A[:, 2:end],1)
+    H₀ = hcat(H₀...)
+    H₁ = hcat(H₁...)
+    return ExactDMD(H₀, H₁)
 
-H*pinv(H2)
+end
