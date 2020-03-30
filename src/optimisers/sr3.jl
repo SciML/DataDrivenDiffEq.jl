86b2697df86b7dd5068160e4e489d2edb10c3059
Julius Martensen <julius.martensen@gmail.com>
2020-03-30 17:34:08 +0200

    Add more info to SR3

--- src/optimisers/sr3.jl
+++ src/optimisers/sr3.jl
@@ -2,11 +2,18 @@
 # A unified sparse optimization framework to learn parsimonious physics-informed models from data
 # by K Champion et. al.
 
-mutable struct SR3{U,T} <: AbstractOptimiser
+mutable struct SR3{U,T} <: AbstractOptimiser where {T <: ProximableFunction}
     λ::U
     ν::U
     R::T
 end
+
+Base.show(io::IO, x::SR3) = print(io, "SR3($(get_threshold(x)), $(x.R))")
+
+@inline function Base.print(io::IO, x::SR3)
+    show(io, x)
+end
+
 
 function SR3(λ = 1e-1, ν = 1.0)
     R = NormL1
@@ -14,11 +21,11 @@ function SR3(λ = 1e-1, ν = 1.0)
 end
 
 function set_threshold!(opt::SR3, threshold)
-    opt.λ = threshold^2*opt.ν /2
+    opt.λ = threshold
     return
 end
 
-get_threshold(opt::SR3) = sqrt(2*opt.λ/opt.ν)
+get_threshold(opt::SR3) = opt.λ
 
 init(o::SR3, A::AbstractArray, Y::AbstractArray) =  A \ Y
 init!(X::AbstractArray, o::SR3, A::AbstractArray, Y::AbstractArray) =  ldiv!(X, qr(A, Val(true)), Y)
@@ -36,12 +43,9 @@ function fit!(X::AbstractArray, A::AbstractArray, Y::AbstractArray, opt::SR3; ma
         # Solve rigde regression
         X .= P*(X̂+W/(opt.ν))
         # Add proximal iteration
-        prox!(W, f, X, opt.ν*opt.λ)
+        prox!(W, f, X)
     end
 
-    # This is the effective threshold of the SR3 algorithm
-    # See Unified Framework paper supplementary material S1
-    #η = sqrt(2*opt.λ*opt.ν)
     X[abs.(X) .< get_threshold(opt)] .= zero(eltype(X))
     return
 end
