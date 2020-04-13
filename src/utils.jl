2c52b73a921d8608d3aee77f86b37eec7b7b12d2
Julius Martensen <julius.martensen@gmail.com>
2020-04-13 11:33:25 +0200

    Adapt AICC and BIC for Matrix / Vector use

--- src/utils.jl
+++ src/utils.jl
@@ -8,18 +8,27 @@ function AIC(k::Int64, X::AbstractArray, Y::AbstractArray; likelyhood = (X,Y) ->
     return 2*k - 2*log(likelyhood(X, Y))
 end
 # Taken from https://royalsocietypublishing.org/doi/pdf/10.1098/rspa.2017.0009
-function AICC(k::Int64, X::AbstractArray, Y::AbstractArray; likelyhood = (X,Y) -> sum(abs2, X-Y))
+function AICC(k::Int64, X::AbstractMatrix, Y::AbstractMatrix; likelyhood = (X,Y) -> sum(abs2, X-Y))
     @assert size(X) == size(Y) "Dimensions of trajectories should be equal !"
     return AIC(k, X, Y, likelyhood = likelyhood)+ 2*(k+1)*(k+2)/(size(X)[2]-k-2)
 end
 
+function AICC(k::Int64, X::AbstractVector, Y::AbstractVector; likelyhood = (X,Y) -> sum(abs2, X-Y))
+    @assert size(X) == size(Y) "Dimensions of trajectories should be equal !"
+    return AIC(k, X, Y, likelyhood = likelyhood)+ 2*(k+1)*(k+2)/(length(X)-k-2)
+end
+
 # Double check on that
 # Taken from https://www.immagic.com/eLibrary/ARCHIVES/GENERAL/WIKIPEDI/W120607B.pdf
-function BIC(k::Int64, X::AbstractArray, Y::AbstractArray; likelyhood = (X,Y) -> sum(abs2, X-Y))
+function BIC(k::Int64, X::AbstractMatrix, Y::AbstractMatrix; likelyhood = (X,Y) -> sum(abs2, X-Y))
     @assert size(X) == size(Y) "Dimensions of trajectories should be equal !"
     return - 2*log(likelyhood(X, Y)) + k*log(size(X)[2])
 end
 
+function BIC(k::Int64, X::AbstractVector, Y::AbstractVector; likelyhood = (X,Y) -> sum(abs2, X-Y))
+    @assert size(X) == size(Y) "Dimensions of trajectories should be equal !"
+    return - 2*log(likelyhood(X, Y)) + k*log(length(X))
+end
 
 # Optimal Shrinkage for data in presence of white noise
 # See D. L. Donoho and M. Gavish, "The Optimal Hard Threshold for Singular
