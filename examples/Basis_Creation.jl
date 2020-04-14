899578019838b982c6d3fec3a79aa80f4449dd10
Julius Martensen <julius.martensen@gmail.com>
2020-04-14 20:44:34 +0200

    Adapt examples

--- examples/Basis_Creation.jl
+++ examples/Basis_Creation.jl
@@ -5,25 +5,32 @@ using ModelingToolkit
 
 # Frist we define a set of variables and parameters
 @variables u[1:3]
-@parameters w[1:2]
+@parameters t w[1:2]
 
 # Now the equations which form our basis
-h = [u[1]; u[2]; cos(w[1]*u[2]+w[2]*u[3]); u[3]+u[2]]
+h = [u[1]; u[2]; cos(w[1]*u[2]+w[2]*u[3])]
 
 # Then we simply create a basis
-b = Basis(h, u, parameters = w)
+b= Basis(h, u, parameters = w)
+ODESystem(b)
 
+# Basis are callable with out of place
+x = b([1;2;3])
+# And in place
+dx = similar(x)
+b(dx, [1;2;3])
+dx
 # And look at the corresponding eqs
 println(b)
 
 # Suppose we want to add another equation, say sin(u[1])
 # The basis behaves like an array
 push!(b, sin(u[1]))
-size(b) # (5)
+size(b) # (4)
 
 # Adding an equation which is already present, does not change the basis
 push!(b, sin(u[1]))
-size(b) # Still 5
+size(b) # Still 4
 
 # We can iterate over the basis
 for bi in b
@@ -33,23 +40,22 @@ end
 # Index specific eqs
 b[3]
 
-# And of course evaluate
-# With fixed parameters
-b([1;2;3], p = [2; 4])
-# And without
-b([1;2;3])
-# Or for trajectories
+# And of course evaluate over trajectories
 X = randn(3, 40)
 Y_p = b(X)
-Y = b(X, p = [2;4])
+# With parameter and time
+t = independent_variable(b)
+push!(b, sin(t))
+Y = b(X, [2;4], 0:39)
 
 
 # This allows you to transform a basis simply via
 @variables x[1:2]
 y = [sin(x[1]); cos(x[1]); x[2]]
-b2 = Basis(b(y), x, parameters = w)
-println(b2)
 
+b2 = Basis(b(y, parameters(b), independent_variable(b)), x, parameters = w, iv = t)
+println(b2)
+b2([1;2;3], [1 3], 0.0)
 
 # We can merge basis
 b3 = merge(b, b2)
@@ -61,6 +67,7 @@ println(b3)
 # Get the variables or parameters
 variables(b)
 parameters(b)
+independent_variable(b)
 
 # We can also check if two bases are equal
 b == b
