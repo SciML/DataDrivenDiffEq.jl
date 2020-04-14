9b73f3e7dd484f40fba5b3543c15f8b4334938ab
Julius Martensen <julius.martensen@gmail.com>
2020-04-14 20:30:13 +0200

    Adapt basis for iip and independent variable

--- test/basis.jl
+++ test/basis.jl
@@ -1,13 +1,14 @@
 
 @testset "Basis" begin
     @variables u[1:3]
-    @parameters w[1:2]
+    @parameters w[1:2] t
     h = [u[1]; u[2]; cos(w[1]*u[2]+w[2]*u[3]); u[3]+u[2]]
     h_not_unique = [u[1]; u[1]; u[1]^1; h]
-    basis = Basis(h_not_unique, u, parameters = w)
+    basis = Basis(h_not_unique, u, parameters = w, iv = t)
 
     @test isequal(variables(basis), u)
     @test isequal(parameters(basis), w)
+    @test isequal(independent_variable(basis), t)
     @test free_parameters(basis) == 6
     @test free_parameters(basis, operations = [+, cos]) == 7
     @test DataDrivenDiffEq.count_operation((ModelingToolkit.Constant(1) + cos(u[2])*sin(u[1]))^3, [+, cos, ^, *]) == 4
@@ -15,7 +16,7 @@
     basis_2 = unique(basis)
     @test isequal(basis, basis_2)
     @test size(basis) == size(h)
-    @test basis([1.0; 2.0; π], p = [0. 1.]) ≈ [1.0; 2.0; -1.0; π+2.0]
+    @test basis([1.0; 2.0; π], [0. 1.]) ≈ [1.0; 2.0; -1.0; π+2.0]
     @test size(basis) == size(basis_2)
     push!(basis_2, sin(u[2]))
     @test size(basis_2)[1] == length(h)+1
@@ -43,8 +44,11 @@
     X[3, :] .= 5*X[3, :]
     # Check the array evaluation
     @test basis(X) ≈ [1.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0] * X
+    Y = similar(X)
+    basis(Y, X)
+    @test Y ≈ [1.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0] * X
     f = jacobian(basis)
-    @test f([1;1;1], [0.0 0.0]) ≈ [1.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0]
+    @test f([1;1;1], [0.0 0.0], 0.0) ≈ [1.0 0.0 0.0; 0.0 0.0 1.0; 0.0 1.0 0.0]
     @test_nowarn sys = ODESystem(basis)
     @test_nowarn [xi for xi in basis]
     @test_nowarn basis[2:end]; basis[2]; first(basis); last(basis); basis[:]
@@ -52,6 +56,6 @@
     @variables u[1:2] t
 
     g = [u[2]; -sin(u[1])*exp(-t)]
-    basis = Basis(g, [u...; t])
-    @test_nowarn ODESystem(basis, t)
+    basis = Basis(g, u, iv = t)
+    @test_nowarn ODESystem(basis)
 end
