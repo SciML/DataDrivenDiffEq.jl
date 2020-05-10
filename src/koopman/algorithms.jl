4ed37785a263cf32c6ab371916e1a35cee39a054
Julius Martensen <julius.martensen@gmail.com>
2020-05-10 07:45:39 +0200

    Adapt docs and check for valid rank reduction

--- src/koopman/algorithms.jl
+++ src/koopman/algorithms.jl
@@ -30,7 +30,8 @@ K = Y*V*Σ*U'
 
 where `Y` and `X = U*Σ*V'` are data matrices.
 If `rtol` ∈ (0, 1) is given, the singular value decomposition is reduced to include only
-entries bigger than `rtol*maximum(Σ)`.
+entries bigger than `rtol*maximum(Σ)`. If `rtol` is an integer, the reduced SVD up to `rtol` is used
+for computation.
 """
 mutable struct DMDSVD{T} <: AbstractKoopmanAlgorithm
     rtol::T
@@ -41,7 +42,7 @@ DMDSVD() = DMDSVD(0.0)
 # Slower but less allocations
 function (x::DMDSVD)(X::AbstractArray, Y::AbstractArray)
     U, S, V = svd(X)
-    if typeof(x.rtol) <: Int
+    if typeof(x.rtol) <: Int && x.rtol < size(X, 1)
         V = V[:, x.rtol]
     elseif zero(x.rtol) < x.rtol < one(x.rtol)
         r = vec(S .> x.rtol*maximum(S))
@@ -59,6 +60,10 @@ end
 Approximates the koopman operator `K` with the algorithm `alg` over the rank reduced data
 matrices `Xᵣ = X Qᵣ` and `Yᵣ = Y Qᵣ` where `Qᵣ` originates from the singular value decomposition of
 the joint data `Z = [X; Y]`. Based on [this paper](http://cwrowley.princeton.edu/papers/Hemati-2017a.pdf).
+
+If `rtol` ∈ (0, 1) is given, the singular value decomposition is reduced to include only
+entries bigger than `rtol*maximum(Σ)`. If `rtol` is an integer, the reduced SVD up to `rtol` is used
+for computation.
 """
 mutable struct TOTALDMD{R, A} <: AbstractKoopmanAlgorithm
     rtol::R
@@ -69,7 +74,7 @@ TOTALDMD() = TOTALDMD(0.0, DMDPINV())
 
 function (x::TOTALDMD)(X::AbstractArray, Y::AbstractArray)
     _ , S, Q = svd([X; Y])
-    if typeof(x.rtol) <: Int
+    if typeof(x.rtol) <: Int && x.rtol < size(X, 1)
         Q = Q[:, 1:x.rtol]
     elseif zero(x.rtol) < x.rtol < one(x.rtol)
         r = vec(S .> x.rtol*maximum(S))
