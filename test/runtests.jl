eb5c8c3620d8883f9d43bf32b4e33dcf27d5294d
AlCap23 <julius.martensen@gmail.com>
2020-07-09 15:47:15 +0200

    Add UODE example as a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,15 @@ using OrdinaryDiffEq
 using Test
 using DataDrivenDiffEq.Optimize
 
+using DiffEqSensitivity
+using Optim
+using DiffEqFlux, Flux
+
+
 include("./basis.jl")
 include("./koopman.jl")
 include("./sindy.jl")
 include("./isindy.jl")
 include("./utils.jl")
 include("./optimize.jl")
+include("./applications/partial_lotka_volterra.jl")
