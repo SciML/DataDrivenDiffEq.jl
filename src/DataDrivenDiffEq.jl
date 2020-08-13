c8971519ed6895f5d8dd9e7f85ea982b030c9d91
AlCap23 <julius.martensen@gmail.com>
2020-08-13 17:36:57 +0200

    Clean up exports

--- src/DataDrivenDiffEq.jl
+++ src/DataDrivenDiffEq.jl
@@ -54,22 +54,6 @@ include("./koopman/extended_dmd.jl")
 export EDMD, gEDMD
 
 
-#include("./exact_dmd.jl")
-#export ExactDMD
-#export eigen, eigvals, eigvecs
-#export modes, frequencies, isstable
-#export dynamics, update!
-#
-#include("./extended_dmd.jl")
-#export ExtendedDMD
-#export dynamics, linear_dynamics
-#export reduce_basis, update!
-
-#include("./dmdc.jl")
-#export DMDc
-#export eigen, eigvals, eigvecs
-#export get_dynamics, get_input_map, dynamics
-
 include("./sindy/results.jl")
 export SparseIdentificationResult
 export print_equations
