ce8805553a94fbce26483e6538c6b7fb2cd0c542
AlCap23 <julius.martensen@gmail.com>
2021-03-13 13:12:08 +0100

    Move generators to utils

--- src/DataDrivenDiffEq.jl
+++ src/DataDrivenDiffEq.jl
@@ -23,6 +23,10 @@ export jacobian
 export free_parameters
 
 
+include("./utils/basis_generators.jl")
+export chebyshev_basis, monomial_basis, polynomial_basis
+export sin_basis, cos_basis, fourier_basis
+
 #include("./optimizers/Optimize.jl")
 #using .Optimize
 #
@@ -32,32 +36,32 @@ export free_parameters
 #export ADM
 
 
-abstract type AbstractKoopmanOperator <: Function end;
-include("./koopman/algorithms.jl")
-export DMDPINV, DMDSVD, TOTALDMD
-
-include("./koopman/koopman.jl")
-export eigen, eigvals, eigvecs
-export modes, frequencies
-export is_discrete, is_continuous
-export operator, generator
-export inputmap, outputmap, updatable, isstable
-
-include("./koopman/linearkoopman.jl")
-export LinearKoopman, update!
-
-include("./koopman/nonlinearkoopman.jl")
-export NonlinearKoopman, reduce_basis
-
-include("./koopman/exact_dmd.jl")
-export DMD, gDMD
-
-include("./koopman/dmdc.jl")
-export DMDc, gDMDc
-
-include("./koopman/extended_dmd.jl")
-export EDMD, gEDMD
-
+#abstract type AbstractKoopmanOperator <: Function end;
+#include("./koopman/algorithms.jl")
+#export DMDPINV, DMDSVD, TOTALDMD
+#
+#include("./koopman/koopman.jl")
+#export eigen, eigvals, eigvecs
+#export modes, frequencies
+#export is_discrete, is_continuous
+#export operator, generator
+#export inputmap, outputmap, updatable, isstable
+#
+#include("./koopman/linearkoopman.jl")
+#export LinearKoopman, update!
+#
+#include("./koopman/nonlinearkoopman.jl")
+#export NonlinearKoopman, reduce_basis
+#
+#include("./koopman/exact_dmd.jl")
+#export DMD, gDMD
+#
+#include("./koopman/dmdc.jl")
+#export DMDc, gDMDc
+#
+#include("./koopman/extended_dmd.jl")
+#export EDMD, gEDMD
+#
 #include("./sindy/results.jl")
 #export SparseIdentificationResult
 #export print_equations
@@ -84,15 +88,12 @@ export EDMD, gEDMD
 
 #include("./system_conversions.jl")
 
-include("./utils.jl")
-export AIC, AICC, BIC
-export optimal_shrinkage, optimal_shrinkage!
-export savitzky_golay
-export burst_sampling, subsample
+#include("./utils.jl")
+#export AIC, AICC, BIC
+#export optimal_shrinkage, optimal_shrinkage!
+#export savitzky_golay
+#export burst_sampling, subsample
 
 
-include("./basis_generators.jl")
-export chebyshev_basis, monomial_basis, polynomial_basis
-export sin_basis, cos_basis, fourier_basis
 
 end # module
