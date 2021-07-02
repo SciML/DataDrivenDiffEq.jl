e2b3dfb983d055b3a6883b66115cc574ac857541
AlCap23 <julius.martensen@gmail.com>
2021-07-02 19:03:47 +0200

    Document sparse regression

--- src/optimizers/sparseregression.jl
+++ src/optimizers/sparseregression.jl
@@ -3,11 +3,17 @@
 $(SIGNATURES)
 
 Implements a sparse regression, given an `AbstractOptimizer` or `AbstractSubspaceOptimizer`.
-`maxiter` indicate the maximum iterations for each call of the optimizer, `abstol` the absolute tolerance of
+`X` denotes the coefficient matrix, `A` the design matrix and `Y` the matrix of observed or target values.
+`X` can be derived via `init(opt, A, Y)`.
+`maxiter` indicates the maximum iterations for each call of the optimizer, `abstol` the absolute tolerance of
 the difference between iterations in the 2 norm. If the optimizer is called with a `Vector` of thresholds, each `maxiter` indicates
 the maximum iterations for each threshold.
 
-If `progress` is set to `true`, a progressbar will be available.
+If `progress` is set to `true`, a progressbar will be available. `progress_outer` and `progress_offset` are used to compute the initial offset of the
+progressbar.
+
+If used with a `Vector` of thresholds, the functions `f` with signature `f(X, A, Y)` and `g` with signature `g(x, threshold) = G(f(X, A, Y))` with the arguments given as stated above can be passed in. These are
+used for finding the pareto-optimal solution to the sparse regression. 
 """
 function sparse_regression!(X, A, Y, opt::AbstractOptimizer{T};
     maxiter::Int = maximum(size(A)),
@@ -83,7 +89,7 @@ function sparse_regression!(X, A, Y, opt::AbstractOptimizer{T};
             )
         end
     end
-    
+
     for i in 1:size(Y, 2)
         @views clip_by_threshold!(X[:, i], λs[i])
     end
