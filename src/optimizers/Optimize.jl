383071325d6b0282244ddc98684252192be5bea2
JuliusMartensen <julius.martensen@gmail.com>
2021-11-22 09:17:16 +0100

    Clean up old code

--- src/optimizers/Optimize.jl
+++ src/optimizers/Optimize.jl
@@ -84,7 +84,7 @@ include("./admm.jl")
 include("./sr3.jl")
 
 #Nullspace for implicit sindy
-include("./adm.jl")
+#include("./adm.jl")
 include("./implicit.jl")
 
 
@@ -103,6 +103,6 @@ include("./sparseregression.jl")
 export sparse_regression!
 export init, init!, set_threshold!, get_threshold
 export STLSQ, ADMM, SR3
-export ImplicitOptimizer, ADM
+export ImplicitOptimizer
 
 end
