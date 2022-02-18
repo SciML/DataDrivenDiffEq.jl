99f78b3f0a22dbf3c54625ff2a054cb741b10a37
Documenter.jl <documenter@juliadocs.github.io>
2022-02-18 09:44:43 +0000

    build based on d3c836e

--- previews/PR335/examples/symbolic_regression.jl
+++ previews/PR335/examples/symbolic_regression.jl
@@ -2,6 +2,7 @@ using DataDrivenDiffEq
 using ModelingToolkit
 using LinearAlgebra
 using OrdinaryDiffEq
+using SymbolicRegression
 
 A = [-0.9 0.2; 0.0 -0.2]
 B = [0.0; 1.0]
@@ -15,42 +16,39 @@ sol = solve(sys, Tsit5(), saveat = 0.05);
 
 X = Array(sol)
 t = sol.t
-control(u,p,t) = [sin(0.5*t)]
-prob = ContinuousDataDrivenProblem(X, t, U = control)
+U = permutedims(sin.(0.5*t))
+prob = ContinuousDataDrivenProblem(X, t, U = U)
 
-res = solve(prob, DMDSVD(), digits = 1)
+alg = EQSearch([+, *, -], loss = L1DistLoss(), maxdepth = 3)
+
+res = solve(prob, alg, numprocs = 0, multithreading = false)
 
 system = result(res)
 
-generator(system)
-
-@parameters t
-@variables x[1:2](t) u[1:1](t)
-
-basis = Basis([x; u], x, controls = u, independent_variable = t, name = :LinearBasis)
-
-sparse_res = solve(prob, basis, STLSQ(1e-1))
-
-sparse_system = result(sparse_res)
+u = controls(system)
+t = get_iv(system)
 
 subs_control = (u[1] => sin(0.5*t))
 
-eqs = map(equations(sparse_system)) do eq
+eqs = map(equations(system)) do eq
     eq.lhs ~ substitute(eq.rhs, subs_control)
 end
 
 @named sys = ODESystem(
     eqs,
-    get_iv(sparse_system),
-    states(sparse_system),
-    parameters(sparse_system)
+    get_iv(system),
+    states(system),
+    []
     );
 
+x = states(system)
 x0 = [x[1] => u0[1], x[2] => u0[2]]
-ps = parameter_map(sparse_res)
 
-ode_prob = ODEProblem(sys, x0, tspan, ps)
+ode_prob = ODEProblem(sys, x0, tspan)
 estimate = solve(ode_prob, Tsit5(), saveat = prob.t);
+
+println(res)
+println(system)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
