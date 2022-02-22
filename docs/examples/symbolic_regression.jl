e715abd511413f048397b6f0f5a347ebae386338
JuliusMartensen <julius.martensen@gmail.com>
2022-02-22 18:30:41 +0100

    Adapt examples

--- docs/examples/symbolic_regression.jl
+++ docs/examples/symbolic_regression.jl
@@ -1,6 +1,10 @@
 # # [Symbolic Regression](@id symbolic_regression_simple)
 # 
-#note # Symbolic regression is using  regularized evolution, simulated annealing, and gradient-free optimization to find suitable equations. Hence, the performance might differ and depends strongly on the hyperparameters of the optimization. This example might not recover the groundtruth, but is showing off the use within `DataDrivenDiffEq.jl`.
+# !!! note 
+#   Symbolic regression is using  regularized evolution, simulated annealing, and gradient-free optimization to find suitable equations. 
+#   Hence, the performance might differ and depends strongly on the hyperparameters of the optimization. 
+#   This example might not recover the groundtruth, but is showing off the use within `DataDrivenDiffEq.jl`.
+#
 # DataDrivenDiffEq offers an interface to [`SymbolicRegression.jl`](https://github.com/MilesCranmer/SymbolicRegression.jl) to infer more complex functions. To 
 # use it, simply load a sufficient version of `SymbolicRegression` (currently we supported version 0.6.14 to 0.6.19).
 
