cd70db2beba5e87e086424783e5b9a72029023f1
JuliusMartensen <julius.martensen@gmail.com>
2022-02-22 10:10:08 +0100

    Adapt output of SR

--- docs/examples/symbolic_regression.jl
+++ docs/examples/symbolic_regression.jl
@@ -34,15 +34,14 @@ prob = ContinuousDataDrivenProblem(X, t, U = U)
 # By default, it takes in a `Vector` of `Functions` and additional [keyworded arguments](https://astroautomata.com/SymbolicRegression.jl/v0.6/api/#Options). We will stick to simple operations 
 # like subtraction and multiplication, use a `L1DistLoss` , limit the maximum size and punish complex equations while fitting our equations on minibatches. 
 
-alg = EQSearch([-, *], loss = L1DistLoss(), maxsize = 9, batching = true, batchSize = 50, parsimony = 0.001f0)
+alg = EQSearch([-, *], loss = L1DistLoss(), verbosity = 0, maxsize = 9, batching = true, batchSize = 50, parsimony = 0.001f0)
 
 # Again, we `solve` the problem to obtain a [`DataDrivenResult`](@ref). Note that any additional keyworded arguments are passed onto 
 # symbolic regressions [`EquationSearch`](https://astroautomata.com/SymbolicRegression.jl/v0.6/api/#EquationSearch) with the exception of `niterations` which 
 # is `max_iter`
 
-res = solve(prob, alg, max_iter = 100, numprocs = 0, multithreading = true)
-#md 
-println(res) 
+res = solve(prob, alg, max_iter = 100, numprocs = 0, multithreading = false)
+#md println(res) 
 
 # We see that the system has been recovered correctly, indicated by the small error. A closer look at the equations r
 
