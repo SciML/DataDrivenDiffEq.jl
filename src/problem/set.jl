8632c1c5725e9116902bdbff44ae356662f9ba85
JuliusMartensen <julius.martensen@gmail.com>
2022-03-22 14:55:11 +0100

    Add oop args

--- src/problem/set.jl
+++ src/problem/set.jl
@@ -119,6 +119,34 @@ function init_implicits(x::DataDrivenDataset{N, W, C}) where {N, W, C}
     )
 end
 
+function init_explicit(x::DataDrivenDataset{N, W, C}) where {N, W, C}
+    first_prob = first(x.probs)
+    n_x, m = size(x)
+    n_u = size(first_prob.U, 1)
+    n_y = size(get_target(first_prob), 1)
+    return (
+        zeros(N, n_x, m),
+        parameters(first_prob),
+        zeros(N, m),
+        n_u > 0 ? zeros(N, n_u, m) : N[]
+    )
+end
+
+function get_oop_args(x::DataDrivenDataset{N,W,C}) where {N,W,C}
+    X, p, t, U = init_explicit(x)
+    last = 1
+    @views for (i, s) in enumerate(cumsum(x.sizes))
+        # Only copy if U is present
+        if !W
+            map(copyto!, (X[:, last:s], p, t[last:s],U[:, last:s]), get_oop_args(x.probs[i]))
+        else
+            map(copyto!, (X[:, last:s], p, t[last:s]), get_oop_args(x.probs[i])[1:3])
+        end
+        last += s
+    end
+    return (X, p, t, U)
+end
+
 function get_implicit_oop_args(x::DataDrivenDataset{N,W,C}) where {N,W,C}
     X, p, t, U = init_implicits(x)
     last = 1
@@ -134,6 +162,7 @@ function get_implicit_oop_args(x::DataDrivenDataset{N,W,C}) where {N,W,C}
     return (X, p, t, U)
 end
 
+
 function (b::AbstractBasis)(d::DataDrivenDataset)
     reduce(hcat, map(b, d.probs))
 end
@@ -146,5 +175,3 @@ function (b::AbstractBasis)(dx::AbstractMatrix, d::DataDrivenDataset)
     end
     return
 end
-
-
