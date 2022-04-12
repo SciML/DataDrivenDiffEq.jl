f013f3c5f34eec677e5fc2759104b79c1419845d
Documenter.jl <documenter@juliadocs.github.io>
2022-04-12 11:12:19 +0000

    build based on 47b75b9

--- dev/examples/5_michaelis_menten.jl
+++ dev/examples/5_michaelis_menten.jl
@@ -10,10 +10,10 @@ end
 
 u0 = [0.5]
 
-problem_1 = ODEProblem(michaelis_menten, u0, (0.0, 4.0))
-solution_1 = solve(problem_1, Tsit5(), saveat = 0.1)
-problem_2 = ODEProblem(michaelis_menten, 2*u0, (4.0, 8.0))
-solution_2 = solve(problem_2, Tsit5(), saveat = 0.1)
+problem_1 = ODEProblem(michaelis_menten, u0, (0.0, 4.0));
+solution_1 = solve(problem_1, Tsit5(), saveat = 0.1);
+problem_2 = ODEProblem(michaelis_menten, 2*u0, (4.0, 8.0));
+solution_2 = solve(problem_2, Tsit5(), saveat = 0.1);
 
 function michaelis_menten(X::AbstractMatrix, p, t::AbstractVector)
     reduce(hcat, map((x,ti)->michaelis_menten(x, p, ti), eachcol(X), t))
@@ -24,8 +24,7 @@ data = (
     Experiment_2 = (X = Array(solution_2), t = solution_2.t, DX = michaelis_menten(Array(solution_2),[], solution_2.t))
 )
 
-
-prob = DataDrivenDiffEq.ContinuousDataset(data)
+prob = DataDrivenDiffEq.ContinuousDataset(data);
 
 @parameters t
 D = Differential(t)
