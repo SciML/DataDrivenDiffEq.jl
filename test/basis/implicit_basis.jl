1b418bad32a6b8095d2dc190e1a332f1274296ac
JuliusMartensen <julius.martensen@gmail.com>
2022-07-27 20:29:07 +0200

    More tests

--- test/basis/implicit_basis.jl
+++ test/basis/implicit_basis.jl
@@ -14,3 +14,10 @@ basis(res_, prob)
 
 @test basis(prob) == res
 @test res_ == res
+
+prob = DiscreteDataDrivenProblem(x) 
+res = x[:, 1:end-1] .+ x[:, 2:end]
+res_ = similar(res)
+basis(res_, prob)
+@test basis(prob) == res
+@test res_ == res
