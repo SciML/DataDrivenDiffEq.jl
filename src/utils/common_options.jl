7cff7f63ad6ed54bdf00a6ab182f3227057fa846
JuliusMartensen <julius.martensen@gmail.com>
2022-08-03 19:26:18 +0200

    Adapt common options slightly

--- src/utils/common_options.jl
+++ src/utils/common_options.jl
@@ -6,6 +6,15 @@ Common options for all methods provided via `DataDrivenDiffEq`.
 # Fields
 $(FIELDS)
     
+## Note
+
+The keyword argument `eval_expression` controls the function creation
+behavior. `eval_expression=true` means that `eval` is used, so normal
+world-age behavior applies (i.e. the functions cannot be called from
+the function that generates them). If `eval_expression=false`,
+then construction via GeneralizedGenerated.jl is utilized to allow for
+same world-age evaluation. However, this can cause Julia to segfault
+on sufficiently large basis functions. By default eval_expression=false.
 """
 @with_kw struct DataDrivenCommonOptions{T, K}
     """Maximum iterations"""
@@ -14,26 +23,20 @@ $(FIELDS)
     abstol::T = sqrt(eps())
     """Relative tolerance"""
     reltol::T = sqrt(eps())
-
-    """Show a progress"""
+    """Show a progress meter"""
     progress::Bool = false
     """Display log - Not implemented right now"""
     verbose::Bool = false
-
-    """Denoise the data using singular value decomposition"""
+    """Denoise the data using the [`optimal threshold`](@ref optimal_shrinkage) method."""
     denoise::Bool = false
     """Normalize the data"""
     normalize::Bool = false
-    #"""Sample options, see `DataSampler`"""
-    #sampler::AbstractSampler = 
-    #
-    #"""Mapping from the candidate solution of a problem to features used for pareto analysis"""
-    #f::Function
-    #"""Scalarization of the features for a candidate solution"""
-    #g::Function
+    """Sample options, see [`DataSampler`](@ref)"""
+    sampler::AbstractSampler = DataSampler()
     """Significant digits for the parameters - used for rounding. Default = 10"""
     digits::Int = 10
-
+    """Evaluate the expression, see [`Symbolics.build_function`](@ref)"""
+    eval_expresssion::Bool = true
     """Additional kwargs"""
     kwargs::K = (;)
 end
