d7c460a562f09299bf726a3226571c3f98e272ea
JuliusMartensen <julius.martensen@gmail.com>
2022-08-12 12:57:03 +0200

    Restructure tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,12 @@ using Test
 
 const GROUP = get(ENV, "GROUP", "All")
 
-@safetestset "Basis" begin include("./basis/basis.jl") end
-@safetestset "Implicit Basis" begin include("./basis/implicit_basis.jl") end
-@safetestset "Basis generators" begin include("./basis/generators.jl") end
-@safetestset "DataDrivenProblem" begin include("./problem/problem.jl") end
-@safetestset "DataDrivenProblem Sampler" begin include("./problem/samplers.jl") end
-@safetestset "DataDrivenSolution" begin include("./solution/solution.jl") end
-@safetestset "Utilities" begin include("./utils.jl") end
+@time begin if GROUP == "All" || GROUP == "Base"
+    @safetestset "Basis" begin include("./basis/basis.jl") end
+    @safetestset "Implicit Basis" begin include("./basis/implicit_basis.jl") end
+    @safetestset "Basis generators" begin include("./basis/generators.jl") end
+    @safetestset "DataDrivenProblem" begin include("./problem/problem.jl") end
+    @safetestset "DataDrivenProblem Sampler" begin include("./problem/samplers.jl") end
+    @safetestset "DataDrivenSolution" begin include("./solution/solution.jl") end
+    @safetestset "Utilities" begin include("./utils.jl") end
+end end
