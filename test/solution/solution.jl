5da0e4a46b21c47f0e1ae65c1ed9516f2a133709
JuliusMartensen <julius.martensen@gmail.com>
2022-10-26 15:50:24 +0200

    Fix solution tests for prob remake

--- test/solution/solution.jl
+++ test/solution/solution.jl
@@ -7,16 +7,16 @@ using DataDrivenDiffEq.CommonSolve
 rng = Random.default_rng()
 Random.seed!(0)
 
+p0 = randn(rng, 2)
 @variables x[1:3]
-@parameters p[1:2]
-
+@parameters p₁=p0[1] p₂=p0[2]
+p = [p₁; p₂]
 eqs = [sin(p[1] * x[1]) + x[2]; p[2] * x[3]]
 
 b = Basis(eqs, x, parameters = p)
-
+@test get_parameter_values(b) == p0
 x = randn(rng, 3, 100)
 t = collect(0.0:1.0:99.0)
-p0 = randn(rng, 2)
 y = b(x, p0, t)
 ŷ = y .+ 0.01 * randn(rng, size(y))
 
@@ -47,8 +47,8 @@ sol = DataDrivenSolution(b, prob, DataDrivenDiffEq.ZeroDataDrivenAlgorithm(),
 @test r2(sol) ≈ 1.0
 @test nobs(sol) == prod(size(y))
 @test_nowarn summarystats(sol)
-
-@test get_problem(sol) == prob
+# We remake the problem now
+@test get_problem(sol) != prob
 @test get_results(sol) == [dummy_sol]
 @test get_algorithm(sol) == DataDrivenDiffEq.ZeroDataDrivenAlgorithm()
 @test isa(get_basis(sol), DataDrivenDiffEq.AbstractBasis)
