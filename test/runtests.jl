f07e37b775fd1cd7bfe9e391d9895045764689d0
JuliusMartensen <julius.martensen@gmail.com>
2022-11-11 11:31:45 +0100

    Adapt tests for subpackages

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,33 @@
-using DataDrivenDiffEq
-using SafeTestsets
-using Test
+using SafeTestsets, Pkg
 
 @info "Finished loading packages"
 
 const GROUP = get(ENV, "GROUP", "All")
 
-@time begin if GROUP == "All" || GROUP == "Base"
-    @safetestset "Basis" begin include("./basis/basis.jl") end
-    @safetestset "Implicit Basis" begin include("./basis/implicit_basis.jl") end
-    @safetestset "Basis generators" begin include("./basis/generators.jl") end
-    @safetestset "DataDrivenProblem" begin include("./problem/problem.jl") end
-    @safetestset "DataDrivenSolution" begin include("./solution/solution.jl") end
-    @safetestset "Utilities" begin include("./utils.jl") end
-    @safetestset "CommonSolve" begin include("./commonsolve/commonsolve.jl") end
-end end
+function dev_subpkg(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.develop(PackageSpec(path = subpkg_path))
+end
+
+function activate_subpkg_env(subpkg)
+    subpkg_path = joinpath(dirname(@__DIR__), "lib", subpkg)
+    Pkg.activate(subpkg_path)
+    Pkg.develop(PackageSpec(path = subpkg_path))
+    Pkg.instantiate()
+end
+
+@time begin 
+    if GROUP == "All" || GROUP == "Core" || GROUP == "Downstream"
+        @safetestset "Basis" begin include("./basis/basis.jl") end
+        @safetestset "Implicit Basis" begin include("./basis/implicit_basis.jl") end
+        @safetestset "Basis generators" begin include("./basis/generators.jl") end
+        @safetestset "DataDrivenProblem" begin include("./problem/problem.jl") end
+        @safetestset "DataDrivenSolution" begin include("./solution/solution.jl") end
+        @safetestset "Utilities" begin include("./utils.jl") end
+        @safetestset "CommonSolve" begin include("./commonsolve/commonsolve.jl") end
+    else
+        dev_subpkg(GROUP)
+        subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
+        Pkg.test(PackageSpec(name = GROUP, path = subpkg_path))
+    end 
+end
