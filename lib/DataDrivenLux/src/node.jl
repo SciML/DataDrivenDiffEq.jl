181a5183b7358add1110a0221bdd3c1a6e490ae4
JuliusMartensen <julius.martensen@gmail.com>
2022-11-19 11:44:59 +0100

    Add Basic Node functionality

---
+++ lib/DataDrivenLux/src/node.jl
@@ -0,0 +1,85 @@
+
+"""
+using Base: ident_cmp
+$(TYPEDEF)
+
+A layer representing a decision node. 
+
+# Fields
+$(FIELDS)
+
+"""
+struct DecisionNode{skip, W, S, F} <: Lux.AbstractExplicitLayer
+    "Input dimensions of the signal"
+    in_dims::Int
+    "Function"
+    f::F
+    "Arity of the function"
+    arity::Int
+    "Weight initialization"
+    init_weight::W
+    "Mapping to the unit simplex"
+    simplex::S
+end 
+
+function DecisionNode(in_dims::Int, arity::Int, f::F = identity; 
+    init_weight=Lux.zeros32, skip = false, 
+    simplex = GumbelSoftmax(), 
+    kwargs...) where F
+    return DecisionNode{skip, typeof(init_weight), typeof(simplex), F}(in_dims, arity, init_weight, simplex,
+                                              f)
+end
+
+function Lux.initialparameters(rng::AbstractRNG, l::DecisionNode)
+    return (; weight=l.init_weight(rng, l.arity, l.in_dims))
+end
+
+Lux.initialstates(rng::AbstractRNG, p::DecisionNode) = begin 
+    (loglikelihood = Float32[],
+    input_id = Int[],
+    temperature = 1f0, 
+    rng = rng)
+end
+
+function update_state(p::DecisionNode, ps, st)
+    @unpack temperature, rng = st
+    @unpack weight = ps
+
+    priors = p.simplex(weight, temperature)
+
+    input_id = map(axes(priors, 1)) do i 
+        dist = Categorical(priors[i,:])
+        id = rand(rng, dist)
+        ll = logpdf(dist, id)
+        return id, ll
+    end    
+
+    merge(st, (;
+        input_id = reduce(vcat, map(first, input_id)),
+        loglikelihood = reduce(vcat, map(last, input_id))
+    ))
+end
+
+function (l::DecisionNode)(x::AbstractArray, ps, st::NamedTuple)
+    st = update_state(l, ps, st)
+    y = _apply_layer(l, x, ps, st)
+    return y, st
+end
+
+
+function (l::DecisionNode{true})(x::AbstractArray, ps, st::NamedTuple)
+    st = update_state(l, ps, st)
+    y = _apply_layer(l, x, ps, st)
+    return vcat(y,x), st
+end
+
+function _apply_layer(l::DecisionNode, x::AbstractMatrix, ps, st)::AbstractMatrix
+    reduce(hcat, map(eachcol(x)) do xi
+        _apply_layer(l, xi, ps, st)
+    end)
+end
+
+function _apply_layer(l::DecisionNode, x::AbstractVector, ps, st)::Number
+    @unpack input_id = st
+    l.f(x[input_id]...)
+end
