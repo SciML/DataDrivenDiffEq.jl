b1f3ca76e8da686b715650651ff1531ca0b22648
Lorenzo Contento <16554887+lcontento@users.noreply.github.com>
2025-05-19 14:56:03 +0200

    fix to make docs pass this may not be the correct thing to do, but parameters inside a DataDrivenProblem do not seem to be really used at the moment

--- src/problem/type.jl
+++ src/problem/type.jl
@@ -152,10 +152,11 @@ function DataDrivenProblem(X::AbstractMatrix;
         DX::AbstractMatrix = Array{eltype(X)}(undef, 0, 0),
         Y::AbstractMatrix = Array{eltype(X)}(undef, 0, 0),
         U::F = Array{eltype(X)}(undef, 0, 0),
-        p::AbstractVector = Array{eltype(X)}(undef, 0),
+        p::Union{AbstractVector, MTKParameters} = Array{eltype(X)}(undef, 0),
         probtype = nothing,
         kwargs...) where {F <: Union{AbstractMatrix, Function}}
-    return DataDrivenProblem(probtype, X, t, DX, Y, U, p; kwargs...)
+    _p = isa(p, MTKParameters) ? p.tunable : p
+    return DataDrivenProblem(probtype, X, t, DX, Y, U, _p; kwargs...)
 end
 
 function Base.summary(io::IO, x::DataDrivenProblem{N, C, P}) where {N, C, P}
