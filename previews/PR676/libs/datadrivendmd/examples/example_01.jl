bdceaf04ab313a2cc71b463e24bf5bb99ad83d84
Documenter.jl <documenter@juliadocs.github.io>
2026-09-07 01:40:57 +0000

    build based on 4423f95

---
+++ previews/PR676/libs/datadrivendmd/examples/example_01.jl
@@ -0,0 +1,24 @@
+using DataDrivenDiffEq
+using LinearAlgebra
+using OrdinaryDiffEq
+using OrdinaryDiffEqFunctionMap
+using SciMLBase: DiscreteProblem
+using DataDrivenDMD
+using StatsAPI: dof, r2, rss
+
+A = [0.9 -0.2; 0.0 0.2]
+u0 = [10.0; -10.0]
+tspan = (0.0, 11.0)
+
+f(u, p, t) = A * u
+
+sys = DiscreteProblem(f, u0, tspan)
+sol = solve(sys, FunctionMap());
+
+prob = DataDrivenProblem(sol)
+
+res = solve(prob, DMDSVD(), digits = 1)
+
+get_basis(res)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
